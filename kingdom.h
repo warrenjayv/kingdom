d5c733cf4d02614b0e71a37608918c24a9180866
U-DESKTOP-0V9L6R9\warre <warrenvineles@gmail.com>
2020-11-01 23:29:08 -0500

    1129 11012020

--- kingdom.h
+++ kingdom.h
@@ -1,7 +1,9 @@
+/*! \file kingdom.h */
 
 #include <gtk/gtk.h>
 
-static void * kng_gtk_activate(GtkAPplication * app, gpointer user_data) 
+static void * kng_gtk_activate(GtkApplication * app, gpointer user_data);
+
 void * kng_gtk_wini(int argc, char **argv); 
 
 
