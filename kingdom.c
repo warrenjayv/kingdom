c3f6aaced09d607be96d298765b43965dba53f1e
U-DESKTOP-0V9L6R9\warre <warrenvineles@gmail.com>
2020-11-01 19:45:04 -0500

    1945 11.01.2020

--- kingdom.c
+++ kingdom.c
@@ -1,2 +1,19 @@
+/*! \file kingdom.c */ 
 
 #include "kingdom.h"
+
+static void activate (GtkApplication * app, gpointer user_data) 
+{
+	window = gtk_application_window_new(app); 	
+	gtk_window_set_title (GTK_WINDOW(window), "window"); 	
+	gtk_window_set_default_size (GTK_WINDOW (window), 200, 200); 
+	gtk_widget_show_all (window); 
+}
+
+void * kng_gtk_wini (int argc, char **argv) {
+	GtkApplication *app; int status; 
+	app = gtk_application_new ("kingdom", G_APPLICATION_FLAGS_NONE); 
+	g_signal_connect(app, "kng_gtk_activate", G_CALLBACK(kng_gtk_activate), NULL); 
+	g_object_unref (app); 
+	return status; 
+}
--- kingdom.h
+++ kingdom.h
@@ -1,1 +1,15 @@
-#include <gtk.h>
+
+#include <gtk/gtk.h>
+
+static void * kng_gtk_activate(GtkAPplication * app, gpointer user_data) 
+void * kng_gtk_wini(int argc, char **argv); 
+
+
+
+
+
+ 
+
+
+
+
